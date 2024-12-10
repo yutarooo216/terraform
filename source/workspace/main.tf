310c29ad1b759c62d4f94dfd19e71e198ebfa31e
yutarooo216 <yutarooo216@gmail.com>
2024-12-10 20:58:48 +0900

    update

--- source/workspace/main.tf
+++ source/workspace/main.tf
@@ -36,3 +36,35 @@ resource "aws_s3_bucket" "private" {
   }
  }
 }
+
+# S3 log
+resource "aws_s3_bucket" "alb_log" {
+        bucket = "alb-log-pragmatic-terraform-yutarooo216"
+        lifecycle_rule {
+                enabled = true
+
+        expiration {
+                days = "180"
+        	}
+	}
+}
+
+# alb s3 policy
+resource "aws_s3_bucket_policy" "alb_log" {
+	bucket = aws_s3_bucket.alb_log.id
+	policy = data.aws_iam_policy_document.alb_log.json
+}
+
+# alb policy document
+data "aws_iam_policy_document" "alb_log" {
+	statement {
+		effect = "Allow"
+		actions = ["s3:PutObject"]
+		resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+	principals {
+		type = "AWS"
+		identifiers = ["582318560864"]
+	}
+ }
+}
