02ed65fede6bb9696fb0382017dea3e888bbedb9
Yutaro Ito <yutarooo216@gmail.com>
2024-12-10 21:16:44 +0900

    update

--- source/workspace/main.tf
+++ source/workspace/main.tf
@@ -36,3 +36,25 @@ resource "aws_s3_bucket" "private" {
   }
  }
 }
+
+# block public access
+resource "aws_s3_bucket_public_access_block" "private" {
+	bucket = aws_s3_bucket.private.id
+
+        block_public_acls = true
+        block_public_policy = true
+        ignore_public_acls = true
+        restrict_public_buckets = true
+}
+
+# piblic s3
+resource "aws_s3_bucket" "public" {
+	bucket = "public-pragmatic-terraterm-yutarooo216"
+	acl = "public-read"
+	cors_rule {
+		allowed_origins = ["https://example.com"]
+		allowed_methods = ["GET"]
+		allowed_headers = ["*"]
+		max_age_seconds = 3000
+	}
+}
